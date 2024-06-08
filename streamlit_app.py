a01b518eae33a7a18ad22440e7840098328888e9
Qasem Nick <qniksefat@gmail.com>
2024-06-08 02:48:17 -0400

    Add two columns for uploaded image and result image

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,35 +9,39 @@ class View:
         st.set_page_config(page_title="Fast MedSAM", page_icon="✂️", layout="wide")
         st.title("Fast Segmentation of Medical Images")
 
-    def show_upload_section(self):
-        st.sidebar.title("Upload Your Image")
-        file = st.sidebar.file_uploader("", type=['jpg', 'png', 'jpeg'])
-        return file
+    def show_upload_section(self, left_column):
+        with left_column:
+            st.sidebar.title("Upload Your Image")
+            file = st.sidebar.file_uploader("", type=['jpg', 'png', 'jpeg'])
+            return file
 
-    def show_samples_section(self):
-        st.sidebar.divider()
-        st.sidebar.title("Use Samples")
-        sample_images = {
-            "Rose": "static/sample1.png",
-            "Daisy": "static/sample2.jpg",
-        }
-        selected_image = st.sidebar.selectbox("Select Image", list(sample_images.keys()))
-        return sample_images[selected_image]
+    def show_samples_section(self, left_column):
+        with left_column:
+            st.sidebar.divider()
+            st.sidebar.title("Use Samples")
+            sample_images = {
+                "Rose": "static/sample1.png",
+                "Daisy": "static/sample2.jpg",
+            }
+            selected_image = st.sidebar.selectbox("Select Image", list(sample_images.keys()))
+            return sample_images[selected_image]
 
-    def show_original_image(self, original_image):
-        st.subheader("Original Image")
-        st.image(original_image)
+    def show_original_image(self, left_column, original_image):
+        with left_column:
+            st.subheader("Original Image")
+            st.image(original_image)
 
-    def show_segmented_image(self, segmented_image):
-        st.subheader("Segmented Image")
-        st.image(segmented_image)
+    def show_segmented_image(self, right_column, segmented_image):
+        with right_column:
+            st.subheader("Segmented Image")
+            st.image(segmented_image)
 
-    def show_download_button(self, image_bytes):
-        st.sidebar.divider()
-        st.sidebar.title("Download Result")
-        st.sidebar.download_button(label="Click to Download", data=image_bytes, 
-                                   file_name="segmented_image.png", mime="image/png")
-
+    def show_download_button(self, right_column, image_bytes):
+        with right_column:
+            st.sidebar.divider()
+            st.sidebar.title("Download Result")
+            st.sidebar.download_button(label="Click to Download", data=image_bytes, 
+                                       file_name="segmented_image.png", mime="image/png")
 
 class Controller:
     def __init__(self):
@@ -56,20 +60,21 @@ class Controller:
             return segmented_image
 
     def run(self):
-        file = self.view.show_upload_section()
+        left_column, right_column = st.columns(2)
+        
+        file = self.view.show_upload_section(left_column)
         if file:
             image = Image.open(file)
         else:
-            sample_image_path = self.view.show_samples_section()
+            sample_image_path = self.view.show_samples_section(left_column)
             image = Image.open(sample_image_path)
 
-        self.view.show_original_image(image)
-
+        self.view.show_original_image(left_column, image)
+        
         segmented_image = self.run_sam(image)
-        self.view.show_segmented_image(segmented_image)
+        self.view.show_segmented_image(right_column, segmented_image)
         image_bytes = self.pil_to_bytes(segmented_image)
-        self.view.show_download_button(image_bytes)
-
+        self.view.show_download_button(right_column, image_bytes)
 
 if __name__ == "__main__":
     controller = Controller()
