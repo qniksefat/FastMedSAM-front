35775b45893277fdf0b6f213f150957b1b62c1e2
Maryam Abdoli <maryam.masinan@syntax.com>
2024-06-09 05:43:16 -0400

    send request to the backend server

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from PIL import Image
 from io import BytesIO
-import time
+import numpy as np
+import requests
+import os
 
+ENDPOINT = os.environ.get("ENDPOINT", "0.0.0.0:8080")
+
+SEGMENT_ENDPOINT = f"http://{ENDPOINT}/segment/"
+UPLOAD_ENDPOINT = f"http://{ENDPOINT}/upload/"
 
 class View:
     def __init__(self):
@@ -11,19 +17,24 @@ class View:
 
     def show_upload_section(self):
         st.sidebar.title("Upload Your Image")
-        file = st.sidebar.file_uploader("Choose an image", label_visibility="collapsed", 
-                                        type=['jpg', 'png', 'jpeg'])
-        return file
+        file = st.sidebar.file_uploader("Choose an image", label_visibility="collapsed",
+                                        type=['npz'])
+        if file is not None:
+            resp = requests.post(UPLOAD_ENDPOINT, files={"file": file.getvalue()})
+            if resp.status_code == 200:
+                st.session_state.image_name = resp.json().get("filename")
+                return file
+            else:
+                st.error("Error uploading image")
+        return None
 
     def show_samples_section(self):
         st.sidebar.title("Or Choose a Sample Image")
         col1, col2 = st.sidebar.columns(2)
-        with col1:
-            if st.sidebar.button("Rose"):
-                return "static/sample1.png"
-        with col2:
-            if st.sidebar.button("Daisy"):
-                return "static/sample2.jpg"
+        if st.sidebar.button("Rose"):
+            return "static/sample1.png"
+        if st.sidebar.button("Daisy"):
+            return "static/sample2.jpg"
         return None
 
     def show_original_image(self, left_column, original_image):
@@ -35,15 +46,13 @@ class View:
         with right_column:
             st.subheader("Segmented Image")
             st.image(segmented_image)
-
+ 
     def show_download_button(self, image_bytes):
-        st.sidebar.divider()
         st.sidebar.title("Download Result")
-        st.sidebar.download_button(label="Click to Download", data=image_bytes, 
+        st.sidebar.download_button(label="Click to Download", data=image_bytes,
                                     file_name="segmented_image.png", mime="image/png")
 
     def show_refresh_button(self):
-        st.sidebar.divider()
         st.sidebar.title("Want to try another image?")
         return st.sidebar.button("Refresh")
 
@@ -59,7 +68,6 @@ class View:
 class Controller:
     def __init__(self):
         self.view = View()
-        
         if 'input_given' not in st.session_state:
             st.session_state.input_given = False
         if 'image' not in st.session_state:
@@ -73,49 +81,60 @@ class Controller:
         byte_im = buf.getvalue()
         return byte_im
 
-    def run_sam(self, image):
+    def run_sam(self, image_name):
         with st.spinner("Segmenting..."):
-            time.sleep(2)   # Simulate a delay
-            segmented_image = image
-            return segmented_image
-        
+            resp = requests.get(SEGMENT_ENDPOINT, params={"filename": image_name})
+            if resp.status_code == 200:
+                segmented_image = Image.open(BytesIO(resp.content))
+                return segmented_image
+            else:
+                st.error("Error during segmentation")
+                return None
+
     def handle_image(self, image):
         if image:
-            image = Image.open(image)
-            st.session_state.image = image
+            st.session_state.image = self.get_mid_layer(image)
             st.session_state.input_given = True
             st.rerun()
 
+    def get_mid_layer(self, npz_file_path):
+        npz_data = np.load(npz_file_path)
+        imgs = npz_data['imgs']
+        mid_layer_index = len(imgs) // 2
+        mid_layer = imgs[mid_layer_index]
+        return mid_layer
+
     def run(self):
         left_column, right_column = st.columns(2)
-        
+
         if not st.session_state.input_given:
             file = self.view.show_upload_section()
             if file:
                 self.handle_image(file)
-            
+
             sample_image_path = self.view.show_samples_section()
             if sample_image_path:
                 self.handle_image(sample_image_path)
-        
         else:
+
             self.view.show_original_image(left_column, st.session_state.image)
-            
+
             st.session_state.text_prompt = self.view.show_prompt_input()
-            
+
             if self.view.show_submit_button():
-                segmented_image = self.run_sam(st.session_state.image)
-                self.view.show_segmented_image(right_column, segmented_image)
-                image_bytes = self.pil_to_bytes(segmented_image)
-                self.view.show_download_button(image_bytes)
-            
+                segmented_image = self.run_sam(st.session_state.image_name)
+                if segmented_image:
+                    self.view.show_segmented_image(right_column, segmented_image)
+                    image_bytes = self.pil_to_bytes(segmented_image)
+                    self.view.show_download_button(image_bytes)
+
             if self.view.show_refresh_button():
                 st.session_state.input_given = False
                 st.session_state.image = None
                 st.session_state.text_prompt = ""
                 st.rerun()
 
-
 if __name__ == "__main__":
     controller = Controller()
     controller.run()
+
