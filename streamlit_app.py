568d864498dbdf3473f65ade265a93e972001665
Qasem Nick <qniksefat@gmail.com>
2024-06-10 16:53:50 -0400

    chore: Update sample files with correct npz files -- error while processing samples

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,16 +32,16 @@ class View:
 
     def show_samples_section(self):
         st.sidebar.title("Or Choose a Sample Image")
-        if st.sidebar.button("Rose"):
-            return "static/sample1.png"
-        if st.sidebar.button("Daisy"):
-            return "static/sample2.jpg"
+        if st.sidebar.button("Sample 1"):
+            return "static/sample1.npz"
+        if st.sidebar.button("Sample 2"):
+            return "static/sample2.npz"
         return None
 
     def show_original_image(self, left_column, original_image):
         with left_column:
             st.subheader("Original Image")
-            idx = len(original_image['imgs']) // 2
+            idx = st.session_state.slice_index if 'slice_index' in st.session_state else len(original_image['imgs']) // 2
             fig = self.visualize_overlay(original_image['imgs'], 
                                          None, None, idx=idx, alpha=.6)
             st.pyplot(fig)
@@ -81,7 +81,7 @@ class View:
     def show_segmented_image(self, right_column, segmented_image):
         with right_column:
             st.subheader("Segmented Image")
-            idx = len(segmented_image['imgs']) // 2
+            idx = st.session_state.slice_index if 'slice_index' in st.session_state else len(segmented_image['imgs']) // 2
             fig = self.visualize_overlay(segmented_image['imgs'], 
                                          None, 
                                          segmented_image['segs'], 
@@ -98,12 +98,15 @@ class View:
         return st.sidebar.button("Refresh")
 
     def show_submit_button(self):
-        return st.sidebar.button("**Segment Image**")
-
-    def show_prompt_input(self):
         st.sidebar.title("Segmentation Parameters")
-        text_prompt = st.sidebar.text_input("Write a candidate label")
-        return text_prompt
+        return st.sidebar.button("**Segment Image**")
+    
+    def show_slice_slider(self, range):
+        st.sidebar.title("Visualizations")
+        with st.sidebar:
+            with st.container(border=True):
+                slice_index = st.slider("Select the slice", 0, range, range // 2)
+                return slice_index
 
 
 class Controller:
@@ -113,14 +116,10 @@ class Controller:
             st.session_state.input_given = False
         if 'image' not in st.session_state:
             st.session_state.image = None
-        if 'text_prompt' not in st.session_state:
-            st.session_state.text_prompt = ""
-
-    def pil_to_bytes(self, img):
-        buf = BytesIO()
-        img.save(buf, format="PNG")
-        byte_im = buf.getvalue()
-        return byte_im
+        if 'slice_index' not in st.session_state:
+            st.session_state.slice_index = 0
+        if 'segmented_image' not in st.session_state:
+            st.session_state.segmented_image = None
 
     def run_sam(self, image_name):
         with st.spinner("Segmenting..."):
@@ -139,8 +138,6 @@ class Controller:
             st.rerun()
 
     def run(self):
-        left_column, right_column = st.columns(2)
-
         if not st.session_state.input_given:
             file = self.view.show_upload_section()
             if file:
@@ -151,22 +148,24 @@ class Controller:
                 self.handle_image(sample_image_path)
                 
         else:
+            slice_range = len(st.session_state.image['imgs']) - 1
+            st.session_state.slice_index = self.view.show_slice_slider(slice_range)
+            
+            left_column, right_column = st.columns(2)
             self.view.show_original_image(left_column, st.session_state.image)
 
-            st.session_state.text_prompt = self.view.show_prompt_input()
-
-            if self.view.show_submit_button():
-                segmented_image = self.run_sam(st.session_state.image_name)
-                if segmented_image:
-                    segmented_image = np.load(segmented_image, allow_pickle=True)
-                    self.view.show_segmented_image(right_column, segmented_image)
-                    # image_bytes = self.pil_to_bytes(segmented_image)
-                    # self.view.show_download_button(image_bytes)
+            if st.session_state.segmented_image is None:
+                if self.view.show_submit_button():
+                    segmented_image = self.run_sam(st.session_state.image_name)
+                    st.session_state.segmented_image = np.load(segmented_image, allow_pickle=True)
+                    st.rerun()
+            else:
+                self.view.show_segmented_image(right_column, st.session_state.segmented_image)
 
             if self.view.show_refresh_button():
                 st.session_state.input_given = False
                 st.session_state.image = None
-                st.session_state.text_prompt = ""
+                st.session_state.slice_index = 0
                 st.rerun()
 
 if __name__ == "__main__":
